20bbbe172b56cf55e020dd39abb9ec64a05813f2
VinayagamKG7 <kvinayagam.dev@gmail.com>
2026-06-08 21:13:06 +0530

    JS Day2 Task Updated

---
+++ JS/DAY2/Task/script.js
@@ -0,0 +1,144 @@
+let userAge = 20;
+console.log(typeof userAge, userAge, "userAge");
+
+let studentName = "Vinayagam";
+console.log(typeof studentName, studentName, "studentName");
+
+let isLoggedIn = true;
+console.log(typeof isLoggedIn, isLoggedIn, "isLoggedIn");
+
+let productPrice = 999;
+console.log(typeof productPrice, productPrice, "productPrice");
+
+let cityName = "Chennai";
+console.log(typeof cityName, cityName, "cityName");
+
+let hasDrivingLicense = true;
+console.log(typeof hasDrivingLicense, hasDrivingLicense, "hasDrivingLicense");
+
+let customerFeedback = null;
+console.log(typeof customerFeedback, customerFeedback, "customerFeedback");
+
+let examResult;
+console.log(typeof examResult, examResult, "examResult");
+
+let courseDuration = 90;
+console.log(typeof courseDuration, courseDuration, "courseDuration");
+
+let departmentName = "IT";
+console.log(typeof departmentName, departmentName, "departmentName");
+
+let isCourseCompleted = true;
+console.log(typeof isCourseCompleted, isCourseCompleted, "isCourseCompleted");
+
+let roomNumber = 205;
+console.log(typeof roomNumber, roomNumber, "roomNumber");
+
+let productName = "Laptop";
+console.log(typeof productName, productName, "productName");
+
+let hasMembership = false;
+console.log(typeof hasMembership, hasMembership, "hasMembership");
+
+let deliveryStatus = null;
+console.log(typeof deliveryStatus, deliveryStatus, "deliveryStatus");
+
+let projectDeadline;
+console.log(typeof projectDeadline, projectDeadline, "projectDeadline");
+
+let totalMarks = 485;
+console.log(typeof totalMarks, totalMarks, "totalMarks");
+
+let browserName = "Chrome";
+console.log(typeof browserName, browserName, "browserName");
+
+let isOnline = true;
+console.log(typeof isOnline, isOnline, "isOnline");
+
+let ticketCount = 12;
+console.log(typeof ticketCount, ticketCount, "ticketCount");
+
+
+
+const accountBalance = 25000;
+console.log(typeof accountBalance, accountBalance, "accountBalance");
+
+const companyName = "TechSoft";
+console.log(typeof companyName, companyName, "companyName");
+
+const isAvailable = true;
+console.log(typeof isAvailable, isAvailable, "isAvailable");
+
+const monthlySalary = 45000;
+console.log(typeof monthlySalary, monthlySalary, "monthlySalary");
+
+const countryName = "India";
+console.log(typeof countryName, countryName, "countryName");
+
+const hasInternetConnection = false;
+console.log(typeof hasInternetConnection, hasInternetConnection, "hasInternetConnection");
+
+const userRemarks = null;
+console.log(typeof userRemarks, userRemarks, "userRemarks");
+
+const profileImage = undefined;
+console.log(typeof profileImage, profileImage, "profileImage");
+
+const websiteVisitors = 5000;
+console.log(typeof websiteVisitors, websiteVisitors, "websiteVisitors");
+
+const courseName = "JavaScript";
+console.log(typeof courseName, courseName, "courseName");
+
+const isPremiumUser = true;
+console.log(typeof isPremiumUser, isPremiumUser, "isPremiumUser");
+
+const officeLocation = "Chennai";
+console.log(typeof officeLocation, officeLocation, "officeLocation");
+
+const isPaymentSuccessful = false;
+console.log(typeof isPaymentSuccessful, isPaymentSuccessful, "isPaymentSuccessful");
+
+const cancellationReason = null;
+console.log(typeof cancellationReason, cancellationReason, "cancellationReason");
+
+const referralCode = undefined;
+console.log(typeof referralCode, referralCode, "referralCode");
+
+const studentCount = 45;
+console.log(typeof studentCount, studentCount, "studentCount");
+
+const appName = "WhatsApp";
+console.log(typeof appName, appName, "appName");
+
+const hasNotifications = true;
+console.log(typeof hasNotifications, hasNotifications, "hasNotifications");
+
+const orderQuantity = 8;
+console.log(typeof orderQuantity, orderQuantity, "orderQuantity");
+
+const languageName = "English";
+console.log(typeof languageName, languageName, "languageName");
+
+
+// Data types
+
+// Number
+let user_age = 20;
+console.log(typeof userAge, userAge, "userAge");
+
+// String
+let student_name = "Vinayagam";
+console.log(typeof studentName, studentName, "studentName");
+
+// Boolean
+let isLogged = true;
+console.log(typeof isLoggedIn, isLoggedIn, "isLoggedIn");
+
+// Undefined
+let mockResult;
+console.log(typeof examResult, mockResult, "examResult");
+
+// Null
+let feedback = null;
+console.log(typeof customerFeedback, customerFeedback, "customerFeedback");
